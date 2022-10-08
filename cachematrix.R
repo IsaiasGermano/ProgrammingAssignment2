4edb379a382f43ce64dd0d8af28dabc5bfc525c7
Isaias Mendes Machado <immgermano@gmail.com>
2022-10-08 00:00:32 -0300

    Task to ProgAssig 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Isaias Mendes Machado
+## My functions do the following: cache a matrix to afterwards used it in more
+## fast way to get your inverse
 
-## Write a short comment describing this function
+## This function returns an object with "subfunctions" from a matrix passed as 
+## argument, doing so, you have a cache to be used once that function have 
+## already been executed. If you want change the original matrix you can use the 
+## set function. After that, you need new running to cache the matrix again.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) m <<- solve
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This function receive the matrix created by the makeCacheMatrix, it checks if 
+## the inverse one exist. If so, just return it. Else, calculates the matrix
+## that is  inverse one.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
